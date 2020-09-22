7e21411adbbeecd2f55729d0369cc3e79d30c1a1
Tom Lynch <tom@trailmarker.io>
2020-09-22 17:22:00 +0930

    updated zipplugin.sh

--- zipplugin.sh
+++ zipplugin.sh
@@ -2,8 +2,12 @@
 
 rm -f nafi-1.0.zip
 mkdir deployment
-git archive master | tar -x -C deployment
+git archive feature/NAFI-Release-Candidate | tar -x -C deployment
 cd deployment
+
+# remove bare image files—these are encoded in resources_rc anyway
+cp nafi/images/icon.png nafi/icon.png
+rm -rf nafi/images
 zip -FSr ../nafi-1.0.zip nafi
 cd ..
 rm -rf deployment
