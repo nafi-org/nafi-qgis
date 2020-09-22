93cde77e7ba9b8d01bf21dce65da643af9faaf01
Tom Lynch <tom@trailmarker.io>
2020-09-22 16:48:59 +0930

    reduce size of fire scar image, make About dialog resizeable, add images to About dialog (NAF-20)

--- nafi/nafi_about_dialog.py
+++ nafi/nafi_about_dialog.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 from qgis.PyQt import QtGui, QtWidgets, uic
+from qgis.PyQt.QtWidgets import QLayout
 
 from .nafi_about_dialog_base import Ui_NafiAboutDialogBase
 
@@ -9,7 +10,6 @@ class NafiAboutDialog(QtWidgets.QDialog, Ui_NafiAboutDialogBase):
     def __init__(self, parent=None):
         """Constructor."""
         super(NafiAboutDialog, self).__init__(parent)
-
         self.setupUi(self)
 
     def accept(self):
