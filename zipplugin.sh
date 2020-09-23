693ed1af09238ee5637451dc142d7557fcf7ab2a
Tom Lynch <tom@trailmarker.io>
2020-09-24 01:35:11 +0930

    updated zip script

--- zipplugin.sh
+++ zipplugin.sh
@@ -2,13 +2,17 @@
 
 rm -f nafi-1.0.zip
 mkdir deployment
-git archive feature/NAFI-Release-Candidate | tar -x -C deployment
+
+# get a clean copy of the current branch in a subdirectory `deployment`
+git archive HEAD | tar -x -C deployment
 cd deployment
 
 # remove bare image files—these are encoded in resources_rc anyway
 cp nafi/images/icon.png nafi/icon.png
 rm -rf nafi/images
-zip -FSr ../nafi-1.0.zip nafi
+
+# zip up the NAFI plug-in directory only into a datestamped archive
+zip -FSr "../nafi-$(date +'%Y%m%d').zip" nafi
 cd ..
 rm -rf deployment
 
