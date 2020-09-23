a12bd3291092198c2371e3a2f23b2d50f9c641bd
Tom Lynch <tom@trailmarker.io>
2020-09-24 01:25:29 +0930

    moved UI files to a separate directory

--- nafi/src/nafi_about_dialog_base.py
+++ nafi/src/nafi_about_dialog_base.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-# Form implementation generated from reading ui file 'src\nafi_about_dialog_base.ui'
+# Form implementation generated from reading ui file 'ui\nafi_about_dialog_base.ui'
 #
 # Created by: PyQt5 UI code generator 5.11.3
 #
--- nafi/src/nafi_dockwidget_base.py
+++ nafi/src/nafi_dockwidget_base.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-# Form implementation generated from reading ui file 'src\nafi_dockwidget_base.ui'
+# Form implementation generated from reading ui file 'ui\nafi_dockwidget_base.ui'
 #
 # Created by: PyQt5 UI code generator 5.11.3
 #
