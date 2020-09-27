9065308d698236eccd39be78e3f2c709d4dfc3f5
Tom Lynch <tom@trailmarker.io>
2020-09-27 17:53:54 +0930

    refactored NafiTreeViewModel to parse capabilities XML for layers directly

--- nafi/src/nafi_tree_view_model.py
+++ nafi/src/nafi_tree_view_model.py
@@ -1,12 +1,14 @@
 # -*- coding: utf-8 -*-
+from collections import OrderedDict
 import re
 
 from qgis.PyQt.QtCore import Qt
 from qgis.PyQt.QtGui import QIcon, QStandardItem, QStandardItemModel 
 
-from owslib.wms import WebMapService
-from owslib.map.wms111 import ContentMetadata, WebMapService_1_1_1
+from owslib.etree import etree
+from owslib.map.wms111 import ContentMetadata 
 
+from .utils import qgsDebug
 from .wms_item import WmsItem
 
 UNWANTED_LAYERS = ["NODATA_RASTER"]
@@ -21,16 +23,39 @@ class NafiTreeViewModel(QStandardItemModel):
     def loadWms(self, wmsUrl, wmsXml, additionalItems=[]):
         """Add an OWSLib WebMapService to this NafiTreeViewModel based on the capabilities XML."""
 
-        # OWSLib uses etree.readfromstring internally, and for some reason,
-        # it can't handle the XML declaration, so it gets hacked off here
+        # etree.fromstring internally for some reason can't handle the XML declaration, 
+        # so it gets hacked off here
         wmsXml = re.sub("<\\?xml.*\\?>", "", wmsXml)
-        wms = WebMapService(url=None, xml=wmsXml)
-        assert isinstance(wms, WebMapService_1_1_1)
+
+        parser = etree.XMLParser(dtd_validation=False, load_dtd=False, no_network=True, recover=True, resolve_entities=False)
+        capabilities = etree.fromstring(wmsXml, parser)
+
+        # recursively gather content metadata for all layer elements, this is stolen 
+        # from OWSLib because it won't let us configure the parser the way we need to
+        # to avoid unwanted network activity, entity resolutions etc
+        # see https://github.com/geopython/OWSLib/blob/8a94500c2137082dfc4e59acd15389312bcb63fb/owslib/map/wms111.py#L113
         
+        # TODO merge this gather with the other tree manipulation functions below
+        contents = OrderedDict()
+        
+        def gatherLayers(parentElement, parentMetadata):
+            layers = []
+            for index, elem in enumerate(parentElement.findall('Layer')):
+                cm = ContentMetadata(elem, parent=parentMetadata,
+                                        index=index + 1,
+                                        parse_remote_metadata=False)
+                if cm.id:
+                    layers.append(cm)
+                    contents[cm.id] = cm
+                cm.children = gatherLayers(elem, cm)
+            return layers
+        
+        gatherLayers(capabilities.find("Capability"), None)
+
         # clear all rows
         self.removeRows(0, self.rowCount())
         # the OWSLib structure is not properly organised via its "children" properties, need to fix it up
-        owsLayers = [wms.contents[layerName] for layerName in list(wms.contents)]
+        owsLayers = [contents[layerName] for layerName in list(contents)]
         # check we've got at least one layer
         assert (len(owsLayers) > 0)
         # calculate our root layer
