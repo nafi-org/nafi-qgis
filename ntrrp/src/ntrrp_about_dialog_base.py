6ba24c3ff148e51415866331da9490f9d3c00afa
Tom <tom@trailmarker.io>
2022-08-26 08:19:33 +0930

    cleanup

--- ntrrp/src/ntrrp_about_dialog_base.py
+++ ntrrp/src/ntrrp_about_dialog_base.py
@@ -8,25 +8,20 @@
 
 from PyQt5 import QtCore, QtGui, QtWidgets
 
-
 class Ui_NtrrpAboutDialogBase(object):
     def setupUi(self, NtrrpAboutDialogBase):
         NtrrpAboutDialogBase.setObjectName("NtrrpAboutDialogBase")
         NtrrpAboutDialogBase.resize(1414, 830)
-        sizePolicy = QtWidgets.QSizePolicy(
-            QtWidgets.QSizePolicy.Preferred, QtWidgets.QSizePolicy.Preferred)
+        sizePolicy = QtWidgets.QSizePolicy(QtWidgets.QSizePolicy.Preferred, QtWidgets.QSizePolicy.Preferred)
         sizePolicy.setHorizontalStretch(0)
         sizePolicy.setVerticalStretch(0)
-        sizePolicy.setHeightForWidth(
-            NtrrpAboutDialogBase.sizePolicy().hasHeightForWidth())
+        sizePolicy.setHeightForWidth(NtrrpAboutDialogBase.sizePolicy().hasHeightForWidth())
         NtrrpAboutDialogBase.setSizePolicy(sizePolicy)
         icon = QtGui.QIcon()
-        icon.addPixmap(QtGui.QPixmap(":/plugins/ntrrp/images/icon.png"),
-                       QtGui.QIcon.Normal, QtGui.QIcon.Off)
+        icon.addPixmap(QtGui.QPixmap(":/plugins/ntrrp/images/icon.png"), QtGui.QIcon.Normal, QtGui.QIcon.Off)
         NtrrpAboutDialogBase.setWindowIcon(icon)
         self.outerVerticalLayout = QtWidgets.QVBoxLayout(NtrrpAboutDialogBase)
-        self.outerVerticalLayout.setSizeConstraint(
-            QtWidgets.QLayout.SetFixedSize)
+        self.outerVerticalLayout.setSizeConstraint(QtWidgets.QLayout.SetFixedSize)
         self.outerVerticalLayout.setObjectName("outerVerticalLayout")
         self.verticalLayout = QtWidgets.QVBoxLayout()
         self.verticalLayout.setSpacing(10)
@@ -37,27 +32,22 @@ class Ui_NtrrpAboutDialogBase(object):
         self.iconLabel.setObjectName("iconLabel")
         self.verticalLayout.addWidget(self.iconLabel)
         self.aboutLabel = QtWidgets.QLabel(NtrrpAboutDialogBase)
-        sizePolicy = QtWidgets.QSizePolicy(
-            QtWidgets.QSizePolicy.Expanding, QtWidgets.QSizePolicy.Expanding)
+        sizePolicy = QtWidgets.QSizePolicy(QtWidgets.QSizePolicy.Expanding, QtWidgets.QSizePolicy.Expanding)
         sizePolicy.setHorizontalStretch(0)
         sizePolicy.setVerticalStretch(0)
-        sizePolicy.setHeightForWidth(
-            self.aboutLabel.sizePolicy().hasHeightForWidth())
+        sizePolicy.setHeightForWidth(self.aboutLabel.sizePolicy().hasHeightForWidth())
         self.aboutLabel.setSizePolicy(sizePolicy)
         self.aboutLabel.setTextFormat(QtCore.Qt.RichText)
-        self.aboutLabel.setAlignment(
-            QtCore.Qt.AlignLeading | QtCore.Qt.AlignLeft | QtCore.Qt.AlignTop)
+        self.aboutLabel.setAlignment(QtCore.Qt.AlignLeading|QtCore.Qt.AlignLeft|QtCore.Qt.AlignTop)
         self.aboutLabel.setWordWrap(True)
         self.aboutLabel.setOpenExternalLinks(True)
         self.aboutLabel.setObjectName("aboutLabel")
         self.verticalLayout.addWidget(self.aboutLabel)
         self.firescarLabel = QtWidgets.QLabel(NtrrpAboutDialogBase)
-        sizePolicy = QtWidgets.QSizePolicy(
-            QtWidgets.QSizePolicy.Preferred, QtWidgets.QSizePolicy.Minimum)
+        sizePolicy = QtWidgets.QSizePolicy(QtWidgets.QSizePolicy.Preferred, QtWidgets.QSizePolicy.Minimum)
         sizePolicy.setHorizontalStretch(0)
         sizePolicy.setVerticalStretch(0)
-        sizePolicy.setHeightForWidth(
-            self.firescarLabel.sizePolicy().hasHeightForWidth())
+        sizePolicy.setHeightForWidth(self.firescarLabel.sizePolicy().hasHeightForWidth())
         self.firescarLabel.setSizePolicy(sizePolicy)
         self.firescarLabel.setMaximumSize(QtCore.QSize(16777215, 400))
         self.firescarLabel.setScaledContents(True)
@@ -77,10 +67,8 @@ class Ui_NtrrpAboutDialogBase(object):
 
     def retranslateUi(self, NtrrpAboutDialogBase):
         _translate = QtCore.QCoreApplication.translate
-        NtrrpAboutDialogBase.setWindowTitle(_translate(
-            "NtrrpAboutDialogBase", "About NAFI Burnt Areas Mapping"))
-        self.iconLabel.setText(_translate(
-            "NtrrpAboutDialogBase", "<html><head/><body><p><img src=\":/plugins/ntrrp/images/icon.png\"/></p></body></html>"))
+        NtrrpAboutDialogBase.setWindowTitle(_translate("NtrrpAboutDialogBase", "About NAFI Burnt Areas Mapping"))
+        self.iconLabel.setText(_translate("NtrrpAboutDialogBase", "<html><head/><body><p><img src=\":/plugins/ntrrp/images/icon.png\"/></p></body></html>"))
         self.aboutLabel.setText(_translate("NtrrpAboutDialogBase", "<html><head/><body><p>This plugin is part of a project to provide high resolution burnt area mapping in northern Australia through the active fire season.</p><p>Burnt area mapping is the cornerstone of successful fire management and is essential for assessing planned burn effectiveness and determining what will stop or slow down a wildfire. Current medium resolution mapping available on the Northern Australia Fire Information website (NAFI) is of limited use for fire management in rural subdivisions. </p><p>This plugin helps through facilitating a semi-automated burnt area mapping process that results in freely available burnt area mapping. The mapping is freely available to fire managers and the general public through the NAFI website and the NAFI mobile application and allows users to undertake near real-time assessments of fire spread risk.</p><p><br/><span style=\" font-weight:600;\">Acknowledgment</span>: The development of the plugin was supported by a Northern Territory Government grant (<a href=\"https://cmc.nt.gov.au/supporting-government/northern-territory-risk-reduction-program\"><span style=\" text-decoration: underline; color:#0000ff;\">NTRRP</span></a>) though the <a href=\"https://www.homeaffairs.gov.au/about-us/our-portfolios/emergency-management/resources\"><span style=\" text-decoration: underline; color:#0000ff;\">National Risk Reduction Framework</span></a> initiative. It initially was produced in partnership with <a href=\"https://depws.nt.gov.au/bushfire-information-and-management/about-bushfires-nt/bushfires-nt\"><span style=\" text-decoration: underline; color:#0000ff;\">Bushfires NT</span></a>, <a href=\"https://www.pfes.nt.gov.au\"><span style=\" text-decoration: underline; color:#0000ff;\">NT Police, Fire &amp; Emergency Services</span></a> and the <a href=\"https://bushfireresearch.org.au/\"><span style=\" text-decoration: underline; color:#0000ff;\">Darwin Centre for Bushfire Research</span></a> to support strategic fuel reduction activities and emergency response for the greater Darwin and Katherine areas.</p></body></html>"))
-        self.firescarLabel.setText(_translate(
-            "NtrrpAboutDialogBase", "<html><head/><body><p><img src=\":/plugins/ntrrp/images/firescar.jpg\"/></p></body></html>"))
+        self.firescarLabel.setText(_translate("NtrrpAboutDialogBase", "<html><head/><body><p><img src=\":/plugins/ntrrp/images/firescar.jpg\"/></p></body></html>"))
+
--- ntrrp/src/ntrrp_dockwidget_base.py
+++ ntrrp/src/ntrrp_dockwidget_base.py
@@ -8,7 +8,6 @@
 
 from PyQt5 import QtCore, QtGui, QtWidgets
 
-
 class Ui_NtrrpDockWidgetBase(object):
     def setupUi(self, NtrrpDockWidgetBase):
         NtrrpDockWidgetBase.setObjectName("NtrrpDockWidgetBase")
@@ -18,20 +17,17 @@ class Ui_NtrrpDockWidgetBase(object):
         font.setWeight(75)
         NtrrpDockWidgetBase.setFont(font)
         icon = QtGui.QIcon()
-        icon.addPixmap(QtGui.QPixmap(":/plugins/ntrrp/images/icon.png"),
-                       QtGui.QIcon.Normal, QtGui.QIcon.Off)
+        icon.addPixmap(QtGui.QPixmap(":/plugins/ntrrp/images/icon.png"), QtGui.QIcon.Normal, QtGui.QIcon.Off)
         NtrrpDockWidgetBase.setWindowIcon(icon)
         self.dockWidgetContents = QtWidgets.QWidget()
         self.dockWidgetContents.setObjectName("dockWidgetContents")
         self.gridLayout = QtWidgets.QGridLayout(self.dockWidgetContents)
         self.gridLayout.setObjectName("gridLayout")
         self.regionLabel = QtWidgets.QLabel(self.dockWidgetContents)
-        sizePolicy = QtWidgets.QSizePolicy(
-            QtWidgets.QSizePolicy.Fixed, QtWidgets.QSizePolicy.Fixed)
+        sizePolicy = QtWidgets.QSizePolicy(QtWidgets.QSizePolicy.Fixed, QtWidgets.QSizePolicy.Fixed)
         sizePolicy.setHorizontalStretch(0)
         sizePolicy.setVerticalStretch(0)
-        sizePolicy.setHeightForWidth(
-            self.regionLabel.sizePolicy().hasHeightForWidth())
+        sizePolicy.setHeightForWidth(self.regionLabel.sizePolicy().hasHeightForWidth())
         self.regionLabel.setSizePolicy(sizePolicy)
         self.regionLabel.setMinimumSize(QtCore.QSize(100, 0))
         self.regionLabel.setMaximumSize(QtCore.QSize(200, 16777215))
@@ -39,8 +35,7 @@ class Ui_NtrrpDockWidgetBase(object):
         font.setBold(True)
         font.setWeight(75)
         self.regionLabel.setFont(font)
-        self.regionLabel.setAlignment(
-            QtCore.Qt.AlignRight | QtCore.Qt.AlignTrailing | QtCore.Qt.AlignVCenter)
+        self.regionLabel.setAlignment(QtCore.Qt.AlignRight|QtCore.Qt.AlignTrailing|QtCore.Qt.AlignVCenter)
         self.regionLabel.setObjectName("regionLabel")
         self.gridLayout.addWidget(self.regionLabel, 0, 1, 1, 1)
         self.regionComboBox = QtWidgets.QComboBox(self.dockWidgetContents)
@@ -50,118 +45,94 @@ class Ui_NtrrpDockWidgetBase(object):
         self.treeView.setObjectName("treeView")
         self.gridLayout.addWidget(self.treeView, 1, 0, 1, 8)
         self.aboutButton = QtWidgets.QPushButton(self.dockWidgetContents)
-        sizePolicy = QtWidgets.QSizePolicy(
-            QtWidgets.QSizePolicy.Fixed, QtWidgets.QSizePolicy.Fixed)
+        sizePolicy = QtWidgets.QSizePolicy(QtWidgets.QSizePolicy.Fixed, QtWidgets.QSizePolicy.Fixed)
         sizePolicy.setHorizontalStretch(0)
         sizePolicy.setVerticalStretch(0)
-        sizePolicy.setHeightForWidth(
-            self.aboutButton.sizePolicy().hasHeightForWidth())
+        sizePolicy.setHeightForWidth(self.aboutButton.sizePolicy().hasHeightForWidth())
         self.aboutButton.setSizePolicy(sizePolicy)
         self.aboutButton.setText("")
         icon1 = QtGui.QIcon()
-        icon1.addPixmap(QtGui.QPixmap(
-            ":/plugins/ntrrp/images/info.png"), QtGui.QIcon.Normal, QtGui.QIcon.Off)
+        icon1.addPixmap(QtGui.QPixmap(":/plugins/ntrrp/images/info.png"), QtGui.QIcon.Normal, QtGui.QIcon.Off)
         self.aboutButton.setIcon(icon1)
         self.aboutButton.setObjectName("aboutButton")
         self.gridLayout.addWidget(self.aboutButton, 2, 0, 1, 1)
         self.refreshButton = QtWidgets.QPushButton(self.dockWidgetContents)
-        sizePolicy = QtWidgets.QSizePolicy(
-            QtWidgets.QSizePolicy.Fixed, QtWidgets.QSizePolicy.Fixed)
+        sizePolicy = QtWidgets.QSizePolicy(QtWidgets.QSizePolicy.Fixed, QtWidgets.QSizePolicy.Fixed)
         sizePolicy.setHorizontalStretch(0)
         sizePolicy.setVerticalStretch(0)
-        sizePolicy.setHeightForWidth(
-            self.refreshButton.sizePolicy().hasHeightForWidth())
+        sizePolicy.setHeightForWidth(self.refreshButton.sizePolicy().hasHeightForWidth())
         self.refreshButton.setSizePolicy(sizePolicy)
         self.refreshButton.setText("")
         icon2 = QtGui.QIcon()
-        icon2.addPixmap(QtGui.QPixmap(
-            ":/plugins/ntrrp/images/refresh.png"), QtGui.QIcon.Normal, QtGui.QIcon.Off)
+        icon2.addPixmap(QtGui.QPixmap(":/plugins/ntrrp/images/refresh.png"), QtGui.QIcon.Normal, QtGui.QIcon.Off)
         self.refreshButton.setIcon(icon2)
         self.refreshButton.setObjectName("refreshButton")
         self.gridLayout.addWidget(self.refreshButton, 2, 2, 1, 1)
         self.downloadButton = QtWidgets.QPushButton(self.dockWidgetContents)
-        sizePolicy = QtWidgets.QSizePolicy(
-            QtWidgets.QSizePolicy.Fixed, QtWidgets.QSizePolicy.Fixed)
+        sizePolicy = QtWidgets.QSizePolicy(QtWidgets.QSizePolicy.Fixed, QtWidgets.QSizePolicy.Fixed)
         sizePolicy.setHorizontalStretch(0)
         sizePolicy.setVerticalStretch(0)
-        sizePolicy.setHeightForWidth(
-            self.downloadButton.sizePolicy().hasHeightForWidth())
+        sizePolicy.setHeightForWidth(self.downloadButton.sizePolicy().hasHeightForWidth())
         self.downloadButton.setSizePolicy(sizePolicy)
         self.downloadButton.setText("")
         icon3 = QtGui.QIcon()
-        icon3.addPixmap(QtGui.QPixmap(
-            ":/plugins/ntrrp/images/download.png"), QtGui.QIcon.Normal, QtGui.QIcon.Off)
+        icon3.addPixmap(QtGui.QPixmap(":/plugins/ntrrp/images/download.png"), QtGui.QIcon.Normal, QtGui.QIcon.Off)
         self.downloadButton.setIcon(icon3)
         self.downloadButton.setObjectName("downloadButton")
         self.gridLayout.addWidget(self.downloadButton, 2, 3, 1, 1)
-        self.currentMappingButton = QtWidgets.QPushButton(
-            self.dockWidgetContents)
-        sizePolicy = QtWidgets.QSizePolicy(
-            QtWidgets.QSizePolicy.Fixed, QtWidgets.QSizePolicy.Fixed)
+        self.currentMappingButton = QtWidgets.QPushButton(self.dockWidgetContents)
+        sizePolicy = QtWidgets.QSizePolicy(QtWidgets.QSizePolicy.Fixed, QtWidgets.QSizePolicy.Fixed)
         sizePolicy.setHorizontalStretch(0)
         sizePolicy.setVerticalStretch(0)
-        sizePolicy.setHeightForWidth(
-            self.currentMappingButton.sizePolicy().hasHeightForWidth())
+        sizePolicy.setHeightForWidth(self.currentMappingButton.sizePolicy().hasHeightForWidth())
         self.currentMappingButton.setSizePolicy(sizePolicy)
         self.currentMappingButton.setText("")
         icon4 = QtGui.QIcon()
-        icon4.addPixmap(QtGui.QPixmap(
-            ":/plugins/ntrrp/images/mapdownload.png"), QtGui.QIcon.Normal, QtGui.QIcon.Off)
+        icon4.addPixmap(QtGui.QPixmap(":/plugins/ntrrp/images/mapdownload.png"), QtGui.QIcon.Normal, QtGui.QIcon.Off)
         self.currentMappingButton.setIcon(icon4)
         self.currentMappingButton.setObjectName("currentMappingButton")
         self.gridLayout.addWidget(self.currentMappingButton, 2, 4, 1, 1)
         self.createButton = QtWidgets.QPushButton(self.dockWidgetContents)
-        sizePolicy = QtWidgets.QSizePolicy(
-            QtWidgets.QSizePolicy.Fixed, QtWidgets.QSizePolicy.Fixed)
+        sizePolicy = QtWidgets.QSizePolicy(QtWidgets.QSizePolicy.Fixed, QtWidgets.QSizePolicy.Fixed)
         sizePolicy.setHorizontalStretch(0)
         sizePolicy.setVerticalStretch(0)
-        sizePolicy.setHeightForWidth(
-            self.createButton.sizePolicy().hasHeightForWidth())
+        sizePolicy.setHeightForWidth(self.createButton.sizePolicy().hasHeightForWidth())
         self.createButton.setSizePolicy(sizePolicy)
         self.createButton.setText("")
         icon5 = QtGui.QIcon()
-        icon5.addPixmap(QtGui.QPixmap(
-            ":/plugins/ntrrp/images/edit.png"), QtGui.QIcon.Normal, QtGui.QIcon.Off)
+        icon5.addPixmap(QtGui.QPixmap(":/plugins/ntrrp/images/edit.png"), QtGui.QIcon.Normal, QtGui.QIcon.Off)
         self.createButton.setIcon(icon5)
         self.createButton.setObjectName("createButton")
         self.gridLayout.addWidget(self.createButton, 2, 5, 1, 1)
         self.approveButton = QtWidgets.QPushButton(self.dockWidgetContents)
-        sizePolicy = QtWidgets.QSizePolicy(
-            QtWidgets.QSizePolicy.Fixed, QtWidgets.QSizePolicy.Fixed)
+        sizePolicy = QtWidgets.QSizePolicy(QtWidgets.QSizePolicy.Fixed, QtWidgets.QSizePolicy.Fixed)
         sizePolicy.setHorizontalStretch(0)
         sizePolicy.setVerticalStretch(0)
-        sizePolicy.setHeightForWidth(
-            self.approveButton.sizePolicy().hasHeightForWidth())
+        sizePolicy.setHeightForWidth(self.approveButton.sizePolicy().hasHeightForWidth())
         self.approveButton.setSizePolicy(sizePolicy)
         self.approveButton.setText("")
         icon6 = QtGui.QIcon()
-        icon6.addPixmap(QtGui.QPixmap(
-            ":/plugins/ntrrp/images/approve.png"), QtGui.QIcon.Normal, QtGui.QIcon.Off)
+        icon6.addPixmap(QtGui.QPixmap(":/plugins/ntrrp/images/approve.png"), QtGui.QIcon.Normal, QtGui.QIcon.Off)
         self.approveButton.setIcon(icon6)
         self.approveButton.setObjectName("approveButton")
         self.gridLayout.addWidget(self.approveButton, 2, 6, 1, 1)
         self.uploadButton = QtWidgets.QPushButton(self.dockWidgetContents)
-        sizePolicy = QtWidgets.QSizePolicy(
-            QtWidgets.QSizePolicy.Fixed, QtWidgets.QSizePolicy.Fixed)
+        sizePolicy = QtWidgets.QSizePolicy(QtWidgets.QSizePolicy.Fixed, QtWidgets.QSizePolicy.Fixed)
         sizePolicy.setHorizontalStretch(0)
         sizePolicy.setVerticalStretch(0)
-        sizePolicy.setHeightForWidth(
-            self.uploadButton.sizePolicy().hasHeightForWidth())
+        sizePolicy.setHeightForWidth(self.uploadButton.sizePolicy().hasHeightForWidth())
         self.uploadButton.setSizePolicy(sizePolicy)
         self.uploadButton.setText("")
         icon7 = QtGui.QIcon()
-        icon7.addPixmap(QtGui.QPixmap(
-            ":/plugins/ntrrp/images/upload.png"), QtGui.QIcon.Normal, QtGui.QIcon.Off)
+        icon7.addPixmap(QtGui.QPixmap(":/plugins/ntrrp/images/upload.png"), QtGui.QIcon.Normal, QtGui.QIcon.Off)
         self.uploadButton.setIcon(icon7)
         self.uploadButton.setObjectName("uploadButton")
         self.gridLayout.addWidget(self.uploadButton, 2, 7, 1, 1)
         self.activeSourceLayerLabel = QtWidgets.QLabel(self.dockWidgetContents)
-        sizePolicy = QtWidgets.QSizePolicy(
-            QtWidgets.QSizePolicy.Fixed, QtWidgets.QSizePolicy.Fixed)
+        sizePolicy = QtWidgets.QSizePolicy(QtWidgets.QSizePolicy.Fixed, QtWidgets.QSizePolicy.Fixed)
         sizePolicy.setHorizontalStretch(0)
         sizePolicy.setVerticalStretch(0)
-        sizePolicy.setHeightForWidth(
-            self.activeSourceLayerLabel.sizePolicy().hasHeightForWidth())
+        sizePolicy.setHeightForWidth(self.activeSourceLayerLabel.sizePolicy().hasHeightForWidth())
         self.activeSourceLayerLabel.setSizePolicy(sizePolicy)
         self.activeSourceLayerLabel.setMinimumSize(QtCore.QSize(100, 0))
         self.activeSourceLayerLabel.setMaximumSize(QtCore.QSize(300, 16777215))
@@ -170,17 +141,14 @@ class Ui_NtrrpDockWidgetBase(object):
         font.setBold(True)
         font.setWeight(75)
         self.activeSourceLayerLabel.setFont(font)
-        self.activeSourceLayerLabel.setAlignment(
-            QtCore.Qt.AlignLeft | QtCore.Qt.AlignVCenter)
+        self.activeSourceLayerLabel.setAlignment(QtCore.Qt.AlignLeft|QtCore.Qt.AlignVCenter)
         self.activeSourceLayerLabel.setObjectName("activeSourceLayerLabel")
         self.gridLayout.addWidget(self.activeSourceLayerLabel, 3, 2, 1, 6)
         self.sourceLayerLabel = QtWidgets.QLabel(self.dockWidgetContents)
-        sizePolicy = QtWidgets.QSizePolicy(
-            QtWidgets.QSizePolicy.Fixed, QtWidgets.QSizePolicy.Fixed)
+        sizePolicy = QtWidgets.QSizePolicy(QtWidgets.QSizePolicy.Fixed, QtWidgets.QSizePolicy.Fixed)
         sizePolicy.setHorizontalStretch(0)
         sizePolicy.setVerticalStretch(0)
-        sizePolicy.setHeightForWidth(
-            self.sourceLayerLabel.sizePolicy().hasHeightForWidth())
+        sizePolicy.setHeightForWidth(self.sourceLayerLabel.sizePolicy().hasHeightForWidth())
         self.sourceLayerLabel.setSizePolicy(sizePolicy)
         self.sourceLayerLabel.setMinimumSize(QtCore.QSize(100, 0))
         self.sourceLayerLabel.setMaximumSize(QtCore.QSize(200, 16777215))
@@ -188,21 +156,17 @@ class Ui_NtrrpDockWidgetBase(object):
         font.setBold(True)
         font.setWeight(75)
         self.sourceLayerLabel.setFont(font)
-        self.sourceLayerLabel.setAlignment(
-            QtCore.Qt.AlignRight | QtCore.Qt.AlignTrailing | QtCore.Qt.AlignVCenter)
+        self.sourceLayerLabel.setAlignment(QtCore.Qt.AlignRight|QtCore.Qt.AlignTrailing|QtCore.Qt.AlignVCenter)
         self.sourceLayerLabel.setObjectName("sourceLayerLabel")
         self.gridLayout.addWidget(self.sourceLayerLabel, 3, 1, 1, 1)
-        self.workingLayerComboBox = QtWidgets.QComboBox(
-            self.dockWidgetContents)
+        self.workingLayerComboBox = QtWidgets.QComboBox(self.dockWidgetContents)
         self.workingLayerComboBox.setObjectName("workingLayerComboBox")
         self.gridLayout.addWidget(self.workingLayerComboBox, 4, 2, 1, 6)
         self.workingLayerLabel = QtWidgets.QLabel(self.dockWidgetContents)
-        sizePolicy = QtWidgets.QSizePolicy(
-            QtWidgets.QSizePolicy.Fixed, QtWidgets.QSizePolicy.Fixed)
+        sizePolicy = QtWidgets.QSizePolicy(QtWidgets.QSizePolicy.Fixed, QtWidgets.QSizePolicy.Fixed)
         sizePolicy.setHorizontalStretch(0)
         sizePolicy.setVerticalStretch(0)
-        sizePolicy.setHeightForWidth(
-            self.workingLayerLabel.sizePolicy().hasHeightForWidth())
+        sizePolicy.setHeightForWidth(self.workingLayerLabel.sizePolicy().hasHeightForWidth())
         self.workingLayerLabel.setSizePolicy(sizePolicy)
         self.workingLayerLabel.setMinimumSize(QtCore.QSize(100, 0))
         self.workingLayerLabel.setMaximumSize(QtCore.QSize(200, 16777215))
@@ -210,8 +174,7 @@ class Ui_NtrrpDockWidgetBase(object):
         font.setBold(True)
         font.setWeight(75)
         self.workingLayerLabel.setFont(font)
-        self.workingLayerLabel.setAlignment(
-            QtCore.Qt.AlignRight | QtCore.Qt.AlignTrailing | QtCore.Qt.AlignVCenter)
+        self.workingLayerLabel.setAlignment(QtCore.Qt.AlignRight|QtCore.Qt.AlignTrailing|QtCore.Qt.AlignVCenter)
         self.workingLayerLabel.setObjectName("workingLayerLabel")
         self.gridLayout.addWidget(self.workingLayerLabel, 4, 1, 1, 1)
         NtrrpDockWidgetBase.setWidget(self.dockWidgetContents)
@@ -221,29 +184,17 @@ class Ui_NtrrpDockWidgetBase(object):
 
     def retranslateUi(self, NtrrpDockWidgetBase):
         _translate = QtCore.QCoreApplication.translate
-        NtrrpDockWidgetBase.setToolTip(_translate(
-            "NtrrpDockWidgetBase", "NAFI Burnt Areas Mapping"))
-        NtrrpDockWidgetBase.setWindowTitle(_translate(
-            "NtrrpDockWidgetBase", "NAFI Burnt Areas Mapping"))
-        self.regionLabel.setText(_translate(
-            "NtrrpDockWidgetBase", "<html><head/><body><p><span style=\" font-weight:600;\">Current region</span></p></body></html>"))
-        self.aboutButton.setToolTip(_translate(
-            "NtrrpDockWidgetBase", "About NAFI Burnt Areas Mapping …"))
-        self.refreshButton.setToolTip(_translate(
-            "NtrrpDockWidgetBase", "Refresh Region Layers"))
-        self.downloadButton.setToolTip(_translate(
-            "NtrrpDockWidgetBase", "Download NAFI Segmentation Features"))
-        self.currentMappingButton.setToolTip(_translate(
-            "NtrrpDockWidgetBase", "Download Current Mapping"))
-        self.createButton.setToolTip(_translate(
-            "NtrrpDockWidgetBase", "Create Working Burnt Areas Layer"))
-        self.approveButton.setToolTip(_translate(
-            "NtrrpDockWidgetBase", "Approve Selected Burnt Areas"))
-        self.uploadButton.setToolTip(_translate(
-            "NtrrpDockWidgetBase", "Upload Burnt Areas"))
-        self.activeSourceLayerLabel.setText(_translate(
-            "NtrrpDockWidgetBase", "<html><head/><body><p><span style=\" font-weight:600;\">Select segmentation layer …</span></p></body></html>"))
-        self.sourceLayerLabel.setText(_translate(
-            "NtrrpDockWidgetBase", "<html><head/><body><p><span style=\" font-weight:600;\">Segmentation layer</span></p></body></html>"))
-        self.workingLayerLabel.setText(_translate(
-            "NtrrpDockWidgetBase", "<html><head/><body><p><span style=\" font-weight:600;\">Working layer</span></p></body></html>"))
+        NtrrpDockWidgetBase.setToolTip(_translate("NtrrpDockWidgetBase", "NAFI Burnt Areas Mapping"))
+        NtrrpDockWidgetBase.setWindowTitle(_translate("NtrrpDockWidgetBase", "NAFI Burnt Areas Mapping"))
+        self.regionLabel.setText(_translate("NtrrpDockWidgetBase", "<html><head/><body><p><span style=\" font-weight:600;\">Current region</span></p></body></html>"))
+        self.aboutButton.setToolTip(_translate("NtrrpDockWidgetBase", "About NAFI Burnt Areas Mapping …"))
+        self.refreshButton.setToolTip(_translate("NtrrpDockWidgetBase", "Refresh Region Layers"))
+        self.downloadButton.setToolTip(_translate("NtrrpDockWidgetBase", "Download NAFI Segmentation Features"))
+        self.currentMappingButton.setToolTip(_translate("NtrrpDockWidgetBase", "Download Current Mapping"))
+        self.createButton.setToolTip(_translate("NtrrpDockWidgetBase", "Create Working Burnt Areas Layer"))
+        self.approveButton.setToolTip(_translate("NtrrpDockWidgetBase", "Approve Selected Burnt Areas"))
+        self.uploadButton.setToolTip(_translate("NtrrpDockWidgetBase", "Upload Burnt Areas"))
+        self.activeSourceLayerLabel.setText(_translate("NtrrpDockWidgetBase", "<html><head/><body><p><span style=\" font-weight:600;\">Select segmentation layer …</span></p></body></html>"))
+        self.sourceLayerLabel.setText(_translate("NtrrpDockWidgetBase", "<html><head/><body><p><span style=\" font-weight:600;\">Segmentation layer</span></p></body></html>"))
+        self.workingLayerLabel.setText(_translate("NtrrpDockWidgetBase", "<html><head/><body><p><span style=\" font-weight:600;\">Working layer</span></p></body></html>"))
+
